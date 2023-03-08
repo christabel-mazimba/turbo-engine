00c2bb03837a96b94806b6036800582398a778ad
ngandugilbert <ngandugilbert18@gmail.com>
2023-03-08 16:33:44 +0200

    Updated encrypted

--- cryptography/Encrypt.java
+++ cryptography/Encrypt.java
@@ -1,3 +1,65 @@
+import java.util.*;
+
 public class Encrypt{
-    // I wrote something here
+    private ArrayList<Integer> bucket; //this will hold the list of integers
+    private String userKey;
+    private String encryptedKey;
+
+    //create constructors
+    public Encrypt(){
+        this("");
+
+    }
+
+    public Encrypt(String userKey){
+        this.userKey = userKey;
+    }
+
+    //getters and setters
+
+    //userKey getter
+    public String getUserKey(){
+        return this.userKey;
+    }
+
+    //user key setter
+    private void setUserKey(String userKey){
+        //set the user key in here
+        this.userKey = userKey;
+    }
+
+    //create encryption method
+    public void encrypt(String userKey){
+        //this is where the algorithm is applied
+        if(isValid(userKey)){
+            //create the bucket list
+        }
+
+        //validate the user key
+    }
+
+    //validate user key
+    private boolean isValid(String userKey){
+        if(userKey.matches("[0-9]+") && userKey.length()==4){
+            //create bucket list
+            this.setUserKey(userKey);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //create bucket list
+    private void createList(){
+        int number =0;
+
+        for(int index = 0; index<this.userKey.length();index++){
+            number = Character.getNumericValue(this.userKey.charAt(index));
+            this.bucket.add(number);
+        }
+
+    }
+
+
+
 }
